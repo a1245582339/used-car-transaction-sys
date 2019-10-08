6fdfdcdbb6d18c02fee47490642bcc5d25361df2
lizhipeng <lizhiepeng@koolearn-inc.com>
2019-10-08 16:33:28 +0800

    添加material ui

--- wap/src/App.tsx
+++ wap/src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { login } from './api/user'
 const App: React.FC = () => {
   useEffect(() => {
-    login({ tel: '13333333333', password: '111111' })
+
   }, [])
   return (
     <div className="App">
@@ -19,7 +18,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1
+          Learn React
         </a>
       </header>
     </div>
